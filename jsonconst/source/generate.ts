2d4ea572f9b9661933f47dacaabed345e029af0e
tschaumburg <thomas@schaumburg-it.dk>
2016-10-16 17:27:00 +0200

    .

---
+++ jsonconst/source/generate.ts
@@ -0,0 +1,37 @@
+﻿namespace dk.schaumburgit.jsonconst
+{
+    export class ISchema
+    {
+        type: string;
+        items?: ISchema;
+        $schema?: string;
+        title: string = null;
+        description: string = null;
+        classname?: string = null;
+        generating?: boolean;
+        properties?: { [name: string]: ISchema; };
+        required?: string[];
+    }
+
+    export class Generate
+    {
+        public generate(
+            schema: ISchema,
+            instance: {},
+            language: string,
+            _namespace: string,
+            rootName: string
+        ): [{ filename: string, contents: string }]
+        {
+            if (language.toLowerCase() === 'typescript')
+            {
+                var gen = new GenerateTypescript();
+                return gen.generate(schema, instance, _namespace, rootName);
+            }
+
+            return null;
+        }
+    }
+
+    export var generate = new Generate();
+}
---
+++ jsonconst/source/test.ts
@@ -0,0 +1,61 @@
+﻿    
+var testSchema = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "title": "Root schema.",
+    "description": "An explanation about the purpose of this instance.",
+    "properties": {
+        "name": {
+            "type": "string",
+            "title": "Name schema.",
+            "description": "An explanation about the purpose of this instance."
+        },
+        "images": {
+            "type": "array",
+            "title": "Images schema.",
+            "description": "An explanation about the purpose of this instance.",
+            "items": {
+                "classname": "ImageMetaData",
+                "type": "object",
+                "title": "0 schema.",
+                "description": "An explanation about the purpose of this instance.",
+                "properties": {
+                    "date": {
+                        "type": "string",
+                        "title": "Date schema.",
+                        "description": "An explanation about the purpose of this instance."
+                    },
+                    "photographer": {
+                        "type": "string",
+                        "title": "Photographer schema.",
+                        "description": "An explanation about the purpose of this instance."
+                    }
+                },
+                "required": [
+                    "date",
+                    "photographer"
+                ]
+            }
+        }
+    },
+    "required": [
+        "name",
+        "images"
+    ]
+}
+
+var testValues = {
+    "name": "Album 1",
+    "images": [
+        {
+            "date": "March 72",
+            "photographer": "Thomas"
+        }
+    ]
+}
+
+console.log('Hello world');
+
+var files = new dk.schaumburgit.jsonconst.Generate().generate(testSchema, testValues, "typescript", "test.module", "config");
+
+console.log(files[0].contents);
